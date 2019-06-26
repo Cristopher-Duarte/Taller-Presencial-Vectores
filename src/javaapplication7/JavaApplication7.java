5e928b1a09113a22432cfefe03e19ee948033aa7
Cristopher Duarte <cristopherduarte50@gmail.com>
2019-06-26 14:26:03 -0500

    2:25

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -14,6 +14,8 @@ public class JavaApplication7 {
         System.out.println("¿Cuantos estudiantes hay?");
         u=leer.nextInt();
         int [] nota;
+        double acum=0;
+        int cont=0;
         nota=new int[u];
         estudiante=new String[u];
         for(int x=0;x<estudiante.length;x++){
@@ -21,10 +23,25 @@ public class JavaApplication7 {
             estudiante[x]=leer.next();
             System.out.println("Digite su nota");
             nota[x]=leer.nextInt();
-        }
-       
-        
             
+
+        }
+        System.out.println("La lista de estudiantes es ");         
+        for(int y=0;y<estudiante.length;y++){
+           System.out.println(estudiante[y]);
+           acum=acum+nota[y];
+
+           
+       }
+        System.out.println("El promedio del salon es " + acum/u);
+        
+        for(int xx=0;xx<nota.length;xx++){
+            if(nota[xx]<= 3){
+                cont=cont+1;
+            }
+        }
+        System.out.println("Los estudiantes que pasaron con un minimo de 3 fueron "+cont);
+                    
         
     }
     
